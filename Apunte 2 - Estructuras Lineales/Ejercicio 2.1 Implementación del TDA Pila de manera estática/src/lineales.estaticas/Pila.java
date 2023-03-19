523ca77694a04762729e37c5fd3b4d2629e4a2b7
Leo Bruno <leobruno400@gmail.com>
2023-03-18 21:54:57 -0300

    clase pila

---
+++ Apunte 2 - Estructuras Lineales/Ejercicio 2.1 Implementación del TDA Pila de manera estática/src/lineales.estaticas/Pila.java
@@ -0,0 +1,98 @@
+public class Pila {
+
+    private static final int TAMANIO = 5;
+    private Object[] arreglo;
+    private int tope;
+
+    //Constructor de la pila.
+    public Pila(){
+        this.arreglo = new Object[TAMANIO];
+        this.tope = -1;
+    }
+
+    //---------------------Basicas---------------------
+    
+    public boolean apilar(Object newElem){
+        //Apila un nuevo elemento.
+        boolean sePuedeApilar;
+
+        if(this.tope+1 >= TAMANIO){
+            //EROR: pila llena.
+            sePuedeApilar = false;
+        }else{
+            // Pone el elemento en el tope de la pila e incrementa el tope.
+            this.tope++;
+            this.arreglo[this.tope] = newElem;
+            sePuedeApilar = true;
+        }
+        return sePuedeApilar;
+    }
+
+    public boolean desapilar(){ 
+        //Desapila un elemento (le aplica la eutanasia).
+        boolean borrar = false;
+
+        if(this.tope > -1){
+            //Eutanasia
+            this.arreglo[this.tope] = null;
+            borrar = true;
+            this.tope--;
+        }
+        return borrar;
+    }
+
+    public Object obtenerTope(){
+        //Obtiene el elemento del tope de la pila.
+        Object elemento = null;
+
+        if(this.tope > -1){
+            elemento  = this.arreglo[this.tope];
+        }
+        return elemento;
+    }
+
+    public boolean esVacia(){
+        //Verifica si la pila esta vacia.
+        boolean vacia = true;
+
+        if(this.tope > -1){
+            vacia = false;
+        }
+        return vacia;
+    }
+
+    //---------------------NO Basicas---------------------
+
+    public void vaciar(){
+        //Vacia la pila sin guardar los elementos.
+        int i;
+
+        for (i = this.tope; i > -1 ; i--) {
+            this.arreglo[i] = null;
+        }
+        this.tope = -1;
+    }
+
+    public String toString(){
+        //Imprime los elementos dentro de la pila.
+        String mensaje = "";
+        int i;
+
+        if(this.tope > -1){
+            for (i = 0; i <= this.tope; i++) {
+                mensaje += i+"-"+this.arreglo[i].toString()+"\n";
+            }
+        }else{
+            mensaje = "ERROR: Pila Vacia";
+        }
+        return mensaje;
+    }
+
+    public Pila clone(){
+        //Clona la pila en una nueva.
+        Pila nueva = new Pila();
+        nueva.arreglo = this.arreglo.clone();
+        nueva.tope = this.tope;
+        return nueva;
+    }
+}
