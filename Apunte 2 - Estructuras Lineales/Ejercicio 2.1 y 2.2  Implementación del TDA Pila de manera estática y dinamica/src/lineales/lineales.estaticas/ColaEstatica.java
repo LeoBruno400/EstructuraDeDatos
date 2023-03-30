f90bc2da7bddcf358dc8f03c4c1a08cdf1f1f054
Leo <leobruno400@gmail.com>
2023-03-30 11:54:35 -0300

    a

---
+++ Apunte 2 - Estructuras Lineales/Ejercicio 2.1 y 2.2  Implementación del TDA Pila de manera estática y dinamica/src/lineales/lineales.estaticas/ColaEstatica.java
@@ -0,0 +1,18 @@
+public class ColaEstatica {
+    
+    private Object[] arreglo;
+    private int frente;
+    private int fin;
+    private static final int TAMANIO = 5;
+
+    //Constructor de la cola
+    public ColaEstatica() {
+        this.arreglo = new Object[this.TAMANIO];
+        this.frente = 0;
+        this.fin = 0;
+    }
+
+    // ---------------------Basicas---------------------
+
+
+}
