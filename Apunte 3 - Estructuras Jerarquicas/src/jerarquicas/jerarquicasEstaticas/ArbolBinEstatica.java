6ec1eba29f5ede6b29ed7c1c14235f5473b4e8bb
Leo <leobruno400@gmail.com>
2023-04-21 10:02:46 -0300

    celda bin no anda loco

--- Apunte 3 - Estructuras Jerarquicas/src/jerarquicas/jerarquicasEstaticas/ArbolBinEstatica.java
+++ Apunte 3 - Estructuras Jerarquicas/src/jerarquicas/jerarquicasEstaticas/ArbolBinEstatica.java
@@ -1,18 +1,19 @@
 package jerarquicas.jerarquicasEstaticas;
+import jerarquicas.jerarquicasEstaticas.CeldaBin;
 
 public class ArbolBinEstatica{
 
-    //Atributos
-    private static final int TAM=20;
-    //private CeldaBin[] arbol;
-    //private int raiz;
-
-    //Constructor vacio
-    public ArbolBinEstatica(){
-        //his.arbol = new CeldaBin[TAM];
-        for (int i = 0; i < TAM; i++) {
-            //this.arbol[i] = new CeldaBin();
-        }
-        //this.raiz = -1;
-    }
+     //Atributos
+     private static final int TAM = 20;
+     private CeldaBin[] arbol;
+     private int raiz;
+ 
+     //Constructor
+     public ArbolBin(){
+         this.arbol = new CeldaBin[TAM];
+         for (int i = 0; i < TAM; i++) {
+             this.arbol[i] = new CeldaBin();
+         }
+         this.raiz = -1;
+     }
 }
